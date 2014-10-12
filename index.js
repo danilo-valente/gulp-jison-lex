31d4bbf497e7289293adba806aab09bba29a928e
Danilo Valente <danilovalente96@gmail.com>
2014-10-12 02:27:40 -0300

    Added template to options.outFile and options.moduleName

--- index.js
+++ index.js
@@ -3,8 +3,23 @@ var gutil = require('gulp-util');
 var through = require('through2');
 var lexParser = require('lex-parser');
 var jisonLex = require('jison-lex');
+var extend = require('extend');
 
 const PLUGIN_NAME = 'gulp-jison-lex';
+
+function template(tmpl, file) {
+    var p = file.history[0];
+    var ext = path.extname(p);
+
+    return gutil.template(tmpl, {
+        file: file,
+        path: {
+            dirname: path.dirname(p),
+            basename: path.basename(p, ext),
+            extname: ext
+        }
+    });
+}
 
 module.exports = function (options) {
     options = options || {};
@@ -22,10 +37,16 @@ module.exports = function (options) {
 
         if (file.isBuffer()) {
 
+            var jlOptions = {
+                moduleName: template(options.moduleName, file),
+                moduleType: options.moduleType
+            };
+
             file = file.clone();
 
-            file.path = options.outFile
-                ? path.resolve(path.dirname(file.path), './', options.outFile)
+            var outFile = template(options.outFile, file);
+            file.path = outFile
+                ? path.resolve(path.dirname(file.path), './', outFile)
                 : gutil.replaceExtension(file.path, '.js');
 
             try {
@@ -36,9 +57,7 @@ module.exports = function (options) {
                     grammar = lexParser.parse(grammar);
                 }
 
-                grammar.options = grammar.options || {};
-                grammar.options.moduleName = options.moduleName;
-                grammar.options.moduleType = options.moduleType;
+                grammar.options = extend(grammar.options, jlOptions);
 
                 var lexer = jisonLex.generate(grammar);
                 file.contents = new Buffer(lexer);
